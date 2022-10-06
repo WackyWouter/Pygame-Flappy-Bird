a624500f4f5d9ce2c5abdd99199552337c691e61
Wouter Bosch <wfcbosch@gmail.com>
2022-10-06 15:03:34 +0100

    add animation and music

--- bean.py
+++ bean.py
@@ -5,10 +5,19 @@ class Bean (pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        self.image = pygame.image.load('img/player/bean-small1.png').convert_alpha()
+        bean_image_1 = pygame.image.load('img/player/bean-small1.png').convert_alpha()
+        bean_image_2 = pygame.image.load('img/player/bean-small3.png').convert_alpha()
+        self.bean_dead = pygame.image.load('img/player/dead-bean.png').convert_alpha()
+
+        self.bean_fly = [bean_image_1, bean_image_2]
+        self.bean_index = 0
+        self.player_jump = pygame.image.load('img/player/bean-small2.png').convert_alpha()
+
+        self.image = self.bean_fly[self.bean_index]
         self.rect = self.image.get_rect(center = (250, 386))
         self.gravity = 0
         self.mask = pygame.mask.from_surface(self.image)
+
 
     def player_input(self):
         keys = pygame.key.get_pressed()
@@ -19,6 +28,19 @@ class Bean (pygame.sprite.Sprite):
         self.gravity += 1
         self.rect.y += self.gravity
 
-    def update(self):
-        self.player_input()
-        self.apply_gravity()
+    def animation_state(self):
+        self.bean_index += 0.1
+        if self.bean_index >= len(self.bean_fly): self.bean_index = 0
+        self.image = self.bean_fly[int(self.bean_index)]
+
+    def dead(self):
+        self.image = self.bean_dead
+
+    def update(self, game_state):
+        if game_state == 'playing':
+            self.animation_state()
+            self.player_input()
+            self.apply_gravity()
+        else:
+            self.dead()
+        
--- game.py
+++ game.py
@@ -32,7 +32,7 @@ class Game:
 
         # Game over
         self.game_over_surf = pygame.image.load('img/menu/gameover.png').convert_alpha()
-        self.game_over_rect = self.game_over_surf.get_rect(center = (250, 250))
+        self.game_over_rect = self.game_over_surf.get_rect(center = (250, -100))
         self.score_screen_surf = pygame.image.load('img/menu/score.png').convert_alpha()
         self.score_screen_rect = self.score_screen_surf.get_rect(center = (250, 350))
         self.gold_medal_surf = pygame.image.load('img/menu/gold.png').convert_alpha()
@@ -42,9 +42,24 @@ class Game:
         self.reset_game_surf = self.font_small.render('Press space to restart', False, (64,64,64))
         self.reset_game_rect = self.reset_game_surf.get_rect(center = (250, 425))
 
+        # bg Music
+        self.bg_music_channel = pygame.mixer.Channel(1)
+        self.bg_music = pygame.mixer.Sound('audio/music.wav')
+        self.bg_music.set_volume(0.1)
+          # Play the audio forever
+        self.bg_music_channel.play(self.bg_music, loops = -1)
+
+        # Fail music
+        self.fail_music_channel = pygame.mixer.Channel(2)
+        self.fail_music = pygame.mixer.Sound('audio/fail.wav')
+        self.fail_music.set_volume(0.1)
+        self.fail_music_channel.play(self.fail_music)
+        self.fail_music_channel.pause()
+        
         # Bean
         self.bean = pygame.sprite.GroupSingle()
         self.bean.add(Bean())
+        
 
         # Obstacle
         self.obstacle_group = pygame.sprite.Group()
@@ -65,9 +80,9 @@ class Game:
         # If bean touches a beam its game over OR if top or bottom of the screen is touched it's game over
         if pygame.sprite.spritecollide(self.bean.sprite, self.obstacle_group, False, pygame.sprite.collide_mask):
             if pygame.sprite.spritecollide(self.bean.sprite, self.obstacle_group, False, pygame.sprite.collide_mask):
-                return 'game over'
+                return 'animating game over'
         elif (self.bean.sprite.rect.y <= 0 or self.bean.sprite.rect.y >= 661):
-            return 'game over'
+            return 'animating game over'
         return 'playing'
 
     def run(self):
@@ -92,17 +107,24 @@ class Game:
                         self.obstacle_group.add(Obstacle('up')) 
                         self.obstacle_group.add(Obstacle('down')) 
                 
-                else:
+                elif self.game_state == 'game over':
                     # Reset the game when its game over
                     if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                        print('reset')
                         self.bean.sprite.rect.y = 250
                         self.start_time = int(pygame.time.get_ticks() / 1000)
                         self.obstacle_group.empty()
                         self.game_state = 'playing'
+                        self.game_over_rect.y  = -100
+
+                        # reset and load music
+                        self.fail_music_channel.stop()
+                        self.fail_music_channel.play(self.fail_music)
+                        self.fail_music_channel.pause()
+
 
             self.screen.blit(self.sky_surf, (0, 0))
             self.bean.draw(self.screen)
+            
 
             # Menu
             if self.game_state == 'menu':
@@ -110,8 +132,11 @@ class Game:
                 self.screen.blit(self.game_name_surf, self.game_name_rect)
 
             elif self.game_state == 'playing':
+                # bg music
+                self.bg_music_channel.unpause()
+
                 # Bean
-                self.bean.update()
+                self.bean.update(self.game_state)
 
                 # Obstacle
                 self.obstacle_group.draw(self.screen)
@@ -126,7 +151,33 @@ class Game:
                 # Collision
                 self.game_state = self.collision_sprite()
 
+            elif self.game_state == 'animating game over':
+                # fail music
+                self.fail_music_channel.unpause()
+
+                # bg music 
+                self.bg_music_channel.pause()
+
+                # Obstacle
+                self.obstacle_group.draw(self.screen)
+                self.screen.blit(self.floor_surf, self.floor_rect)
+
+                # bean
+                self.bean.update(self.game_state) 
+                # Animate the game over dropping in
+                self.game_over_rect.y += 6
+                self.screen.blit(self.game_over_surf, self.game_over_rect)
+
+                
+
+                if self.game_over_rect.y >= 230:
+                    self.game_state = 'game over'
+                
             else: 
+
+                # bean
+                self.bean.update(self.game_state)
+
                 # Score
                 score_surf = self.font_small.render(str(self.score), False, (64,64,64))
                 score_rect = score_surf.get_rect(center = (335, 327))
@@ -135,6 +186,7 @@ class Game:
                 top_score_rect = top_score_surf.get_rect(center = (335, 367))
 
                 # Show menu on the screen
+                self.game_over_rect.y = 230
                 self.screen.blit(self.game_over_surf, self.game_over_rect)
                 self.screen.blit(self.score_screen_surf, self.score_screen_rect)
                 self.screen.blit(top_score_surf, top_score_rect)
